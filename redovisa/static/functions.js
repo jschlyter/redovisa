7069ea36d26b8d21e0b535b1e44cd2a2edaffc1b
Jakob Schlyter <jakob@kirei.se>
2024-07-17 08:36:25 +0200

    init

---
+++ redovisa/static/functions.js
@@ -0,0 +1,9 @@
+function updateTotal() {
+  var arr = document.getElementsByClassName('amount');
+  var tot = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (parseFloat(arr[i].value))
+      tot += parseFloat(arr[i].value);
+  }
+  document.getElementById('total').innerHTML = tot;
+}
