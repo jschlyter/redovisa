ecc476802162dbff9afcb022fa44ea7e78f1c841
Jakob Schlyter <jakob@kirei.se>
2024-08-06 08:03:21 +0200

    split

--- redovisa/views.py
+++ redovisa/views.py
@@ -1,67 +1,19 @@
 import contextlib
 import logging
-import re
 import smtplib
-import uuid
 from email.message import EmailMessage
 
 from fastapi import APIRouter, Request, UploadFile
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, Field
 
 from .middleware import Session
+from .models import ExpenseReport
 
 RECIPIENT_ACCOUNT_COOKIE = "recipient_account"
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
-
-
-class ExpenseItem(BaseModel):
-    account: int
-    description: str | None
-    amount: float
-
-
-class Recipient(BaseModel):
-    name: str
-    email: str
-    account: int
-
-
-class ExpenseReport(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
-    items: list[ExpenseItem]
-    total_amount: float
-    recipient: Recipient
-
-    @classmethod
-    def from_form(cls, form, session: Session):
-        items = []
-
-        for name in form:
-            if match := re.match(r"^(\d+):account$", name):
-                row = match.group(1)
-
-                if account := form.get(name):
-                    items.append(
-                        ExpenseItem(
-                            account=int(account),
-                            description=form.get(f"{row}:description"),
-                            amount=float(form.get(f"{row}:amount")),
-                        )
-                    )
-
-        return cls(
-            items=items,
-            total_amount=sum([item.amount for item in items]),
-            recipient=Recipient(
-                name=session.name,
-                email=session.email,
-                account=form["recipient_account"],
-            ),
-        )
 
 
 @router.get("/")
