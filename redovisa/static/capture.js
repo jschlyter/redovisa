a4a2697339bdbe5cfdef0fc9b27464fb7a8d908c
Jakob Schlyter <jakob@kirei.se>
2024-10-17 19:46:22 +0200

    revert, but remove capture

--- redovisa/static/capture.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", (event) => {
-  const input = document.querySelector("input#receipts");
-  const button = document.querySelector("button#no_capture");
-
-  button.addEventListener("click", () => {
-    input.removeAttribute("capture");
-  });
-});
