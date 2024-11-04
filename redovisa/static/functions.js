ca43e9380882a085fa737e1419d6420bee5bb0e6
Jakob Schlyter <jakob@kirei.se>
2024-11-04 06:47:43 +0100

    require non-zero total

--- redovisa/static/functions.js
+++ redovisa/static/functions.js
@@ -1,9 +1,19 @@
 function updateTotal() {
-  var arr = document.getElementsByClassName('amount');
+  var arr = document.getElementsByClassName("amount");
   var tot = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (parseFloat(arr[i].value))
-      tot += parseFloat(arr[i].value);
+    if (parseFloat(arr[i].value)) tot += parseFloat(arr[i].value);
   }
-  document.getElementById('total').innerHTML = tot.toFixed(2);
+  document.getElementById("total").innerHTML = tot.toFixed(2);
+  if (tot == 0) {
+    document.getElementById("submit").disabled = true;
+  } else {
+    document.getElementById("submit").disabled = false;
+  }
 }
+
+function disableSubmitDefault() {
+  document.getElementById("submit").disabled = true;
+}
+
+window.onload = disableSubmitDefault;
