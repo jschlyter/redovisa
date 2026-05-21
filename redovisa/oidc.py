a0fe65498ea5ae475a506a307f636a372ea5253b
Jakob Schlyter <jakob@kirei.se>
2026-05-21 22:12:26 +0200

    Upgrade and improve OIDC

--- redovisa/oidc.py
+++ redovisa/oidc.py
@@ -1,5 +1,6 @@
 """OpenID Connect Middleware"""
 
+import email.utils
 import json
 import re
 import time
@@ -13,18 +14,21 @@ from urllib.parse import urljoin
 
 import httpx
 import redis
-from cryptojwt.jwt import JWT
-from cryptojwt.key_bundle import KeyBundle
-from cryptojwt.key_jar import KeyJar
-from cryptojwt.utils import b64d, b64e
 from fastapi import HTTPException, Request
 from fastapi.responses import RedirectResponse
+from jwcrypto.common import base64url_decode, base64url_encode
+from jwcrypto.jwk import JWKSet
+from jwcrypto.jwt import JWT
 from pydantic import BaseModel, Field
 from uvicorn._types import ASGI3Application, ASGIReceiveCallable, ASGISendCallable, Scope
 
 from .logging import get_logger
 
 DEFAULT_SCOPE = ["openid", "email", "profile"]
+
+JWKSET_REFRESH_DEFAULT_INTERVAL = 3600  # 1 hour
+JWKSET_REFRESH_MIN_INTERVAL = 60  # 1 minute
+JWKSET_REFRESH_MAX_INTERVAL = 86400  # 24 hours
 
 
 class Session(BaseModel):
@@ -102,40 +106,99 @@ class OidcMiddleware:
         self.callback_path = callback_path
         self.excluded_paths = set(excluded_paths or [])
         self.excluded_re = re.compile(excluded_re) if excluded_re else None
-        self.redis_client = redis_client or redis.StrictRedis()
+        self.redis_client = redis_client or redis.Redis()
         self.users = users
+
+        if forbidden_path:
+            self.excluded_paths.add(forbidden_path)
 
         self.callback_uri = urljoin(self.base_uri, callback_path)
         self.login_redirect_uri = login_redirect_uri or self.base_uri
         self.logout_redirect_uri = logout_redirect_uri or self.base_uri
 
         self.session = httpx.Client()
+        self.async_session = httpx.AsyncClient()
 
+        self._issuer_keys_expires: float | None = None
         self._configuration = self.get_configuration()
+        self._issuer_keys = self.get_issuer_keys()
 
-        self.key_bundle = KeyBundle(source=self.configuration.jwks_uri)
-
-        self.logger.debug(
+        self.logger.info(
             "Read issuer keys",
-            key_count=len(self.key_bundle.keys() or []),
+            key_count=len(self.issuer_keys.export(as_dict=True).get("keys", [])),
             oidc_issuer=self.configuration.issuer,
             oidc_jwks_uri=self.configuration.jwks_uri,
+            expires=datetime.fromtimestamp(self._issuer_keys_expires, tz=UTC).isoformat(),
         )
-
-        self.key_jar = KeyJar()
-        self.key_jar.add_kb(issuer_id=self.configuration.issuer, kb=self.key_bundle)
-
-        self.jwt = JWT(key_jar=self.key_jar)
 
     @property
     def configuration(self) -> OidcConfiguration:
         return self._configuration
 
+    @property
+    def issuer_keys(self) -> JWKSet:
+        return self._issuer_keys
+
     def get_configuration(self) -> OidcConfiguration:
-        conf = self.to_dict_or_raise(self.session.get(self.configuration_uri))
-        return OidcConfiguration.model_validate(conf)
+        """
+        Fetch the OIDC configuration from the issuer's .well-known endpoint and return it as an
+        OidcConfiguration object.
+        """
+
+        try:
+            response = self.session.get(self.configuration_uri)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            self.logger.error(f"Error fetching OIDC configuration: {exc}")
+            raise OpenIDConnectException(f"Error fetching OIDC configuration: {exc}") from exc
+
+        return OidcConfiguration.model_validate(self.to_dict_or_raise(response))
+
+    def get_issuer_keys(self) -> JWKSet:
+        """Get the JWK set from the issuer's jwks_uri and return it as a JWKSet object."""
+
+        try:
+            response = self.session.get(self.configuration.jwks_uri)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            self.logger.error(f"Error fetching issuer keys: {exc}")
+            raise OpenIDConnectException(f"Error fetching issuer keys: {exc}") from exc
+
+        res = JWKSet.from_json(response.text)
+        self._issuer_keys_expires = self.expires_from_response(response)
+
+        return res
+
+    async def refresh_issuer_keys(self) -> None:
+        """Refresh the issuer's JWK set by fetching it from the jwks_uri."""
+
+        if self._issuer_keys_expires and time.time() < self._issuer_keys_expires:
+            return
+
+        try:
+            response = await self.async_session.get(self.configuration.jwks_uri)
+            response.raise_for_status()
+
+            self._issuer_keys = JWKSet.from_json(response.text)
+            self._issuer_keys_expires = self.expires_from_response(response)
+
+            self.logger.info(
+                "Refreshed issuer keys",
+                key_count=len(self.issuer_keys.export(as_dict=True).get("keys", [])),
+                oidc_issuer=self.configuration.issuer,
+                oidc_jwks_uri=self.configuration.jwks_uri,
+                expires=datetime.fromtimestamp(self._issuer_keys_expires, tz=UTC).isoformat(),
+            )
+        except Exception as exc:
+            self.logger.error(f"Failed to refresh issuer keys: {exc}")
+            self._issuer_keys_expires = time.time() + JWKSET_REFRESH_DEFAULT_INTERVAL
 
     async def __call__(self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
+        """
+        ASGI entry point for the middleware, which handles authentication and session management
+        for incoming HTTP requests.
+        """
+
         if scope["type"] == "http":
             path = scope.get("path")
             request = Request(scope)
@@ -187,13 +250,13 @@ class OidcMiddleware:
         if not (state := request.query_params.get("state")):
             raise HTTPException(status_code=400, detail="Authorization state missing")
 
-        state_payload = json.loads(b64d(state.encode()))
+        state_payload = json.loads(base64url_decode(state))
         session_id = state_payload["session_id"]
         if request.cookies.get(self.cookie) != session_id:
             raise HTTPException(status_code=400, detail="Authorization state mismatch")
         login_redirect_uri = state_payload["next"] or self.login_redirect_uri
 
-        claims: dict[str, str | int] = self.authenticate(code, self.callback_uri)
+        claims: dict[str, str | int] = await self.authenticate(code, self.callback_uri)
 
         self.logger.info("Authenticated", claims=claims)
 
@@ -242,14 +305,13 @@ class OidcMiddleware:
         # create state
         session_id = str(uuid.uuid4())
         state_payload = {"next": next, "session_id": session_id}
-        state = b64e(json.dumps(state_payload).encode()).decode()
+        state = base64url_encode(json.dumps(state_payload).encode())
 
         self.logger.debug("Prepare redirect to OP", redirect_uri=self.callback_uri, state_payload=state_payload)
         redirect_url = self.get_auth_redirect_uri(self.callback_uri, state=state)
         self.logger.debug("Redirect to OP", url=redirect_url)
 
         response = RedirectResponse(redirect_url)
-
         response.set_cookie(key=self.cookie, value=session_id, max_age=self.auth_ttl)
 
         return response
@@ -261,25 +323,41 @@ class OidcMiddleware:
         if session_id := request.cookies.get(self.cookie):
             self.redis_client.delete(Session.get_cache_key(session_id))
             response.set_cookie(self.cookie, "", expires=0)
+
         return response
 
     async def get_session(self, request: Request) -> Session | None:
+        """
+        Get the session from the request cookies and return it as a Session object,
+        or None if no valid session is found.
+        """
+
         if session_id := request.cookies.get(self.cookie):  # noqa
             if session_data := self.redis_client.get(Session.get_cache_key(session_id)):
                 return Session.model_validate_json(session_data.decode())
 
-    def authenticate(self, code: str, callback_uri: str, get_user_info: bool = False) -> dict:
-        token = self.get_token(code, callback_uri)
-        self.logger.debug("Received token %s", token)
+    async def authenticate(self, code: str, callback_uri: str, get_user_info: bool = False) -> dict:
+        """Authenticate the user by exchanging the authorization code for a token and optionally fetching user info."""
+
+        token = await self.get_token(code, callback_uri)
+        self.logger.debug(f"Received token {token}")
+
         if get_user_info:
             access_token = token["access_token"]
-            return self.get_user_info(access_token=access_token)
+            return await self.get_user_info(access_token=access_token)
         else:
+            await self.refresh_issuer_keys()
             id_token = token["id_token"]
-            jwt = self.jwt.unpack(id_token)
-            return dict(jwt)
+            decoded_jwt = JWT(
+                jwt=id_token,
+                key=self.issuer_keys,
+                algs=self.configuration.id_token_signing_alg_values_supported,
+            )
+            return json.loads(decoded_jwt.claims)
 
     def get_auth_redirect_uri(self, callback_uri: str, **kwargs) -> str:
+        """Return the OIDC authentication redirect URI with the given callback URI and additional query parameters."""
+
         params = urllib.parse.urlencode(
             {
                 "response_type": "code",
@@ -291,7 +369,9 @@ class OidcMiddleware:
         )
         return f"{self.configuration.authorization_endpoint}?{params}"
 
-    def get_token(self, code: str, callback_uri: str) -> dict:
+    async def get_token(self, code: str, callback_uri: str) -> dict:
+        """Exchange the authorization code for a token by making a POST request to the token endpoint."""
+
         authstr = "Basic " + b64encode(f"{self.client_id}:{self.client_secret}".encode()).decode()
         headers = {"Authorization": authstr}
         data = {
@@ -300,21 +380,74 @@ class OidcMiddleware:
             "redirect_uri": callback_uri,
         }
         self.logger.debug("Get token", token_endpoint=self.configuration.token_endpoint)
-        response = self.session.post(self.configuration.token_endpoint, data=data, headers=headers)
+
+        try:
+            response = await self.async_session.post(self.configuration.token_endpoint, data=data, headers=headers)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            self.logger.error(f"Error fetching token: {exc}")
+            raise OpenIDConnectException(f"Error fetching token: {exc}") from exc
+
         return self.to_dict_or_raise(response)
 
-    def get_user_info(self, access_token: str) -> dict:
+    async def get_user_info(self, access_token: str) -> dict:
+        """Fetch user info from the userinfo endpoint using the given access token."""
+
         bearer = f"Bearer {access_token}"
         headers = {"Authorization": bearer}
-        response = self.session.get(self.configuration.userinfo_endpoint, headers=headers)
+
+        try:
+            response = await self.async_session.get(self.configuration.userinfo_endpoint, headers=headers)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            self.logger.error(f"Error fetching user info: {exc}")
+            raise OpenIDConnectException(f"Error fetching user info: {exc}") from exc
+
         return self.to_dict_or_raise(response)
 
     def to_dict_or_raise(self, response: httpx.Response) -> dict:
+        """Return the JSON-decoded response if the status code is 200, otherwise raise an OpenIDConnectException."""
+
         if response.status_code != 200:
             self.logger.error(f"Returned with status {response.status_code}", status=response.status_code)
             raise OpenIDConnectException(f"Status code {response.status_code} for {response.url}")
+
         try:
             return response.json()
         except JSONDecodeError as exc:
             self.logger.error("Unable to decode JSON")
             raise OpenIDConnectException("Was not able to retrieve data from the response") from exc
+
+    def expires_from_response(self, response: httpx.Response) -> float:
+        """
+        Return the expiration time of the HTTP response based on the Expires header,
+        or None if it cannot be determined.
+        """
+
+        if expires := response.headers.get("Expires"):
+            try:
+                exp = email.utils.parsedate_to_datetime(expires).timestamp()
+                return self.jwk_trim_expire(exp)
+            except Exception as exc:
+                self.logger.warning(f"Failed to parse Expires header: {expires}: {exc}")
+
+        return time.time() + JWKSET_REFRESH_DEFAULT_INTERVAL
+
+    def jwk_trim_expire(self, expires: float) -> float:
+        """Trim the expire time of the issuer keys to avoid long intervals between refreshes."""
+
+        now = time.time()
+
+        if expires < now:
+            self.logger.warning("JWK set already expired")
+            return now + JWKSET_REFRESH_DEFAULT_INTERVAL
+
+        if expires - now > JWKSET_REFRESH_MAX_INTERVAL:
+            self.logger.warning("JWK set expire to far in the future, trimmed")
+            return now + JWKSET_REFRESH_MAX_INTERVAL
+
+        if expires - now < JWKSET_REFRESH_MIN_INTERVAL:
+            self.logger.warning("JWK set expire to soon, trimmed")
+            return now + JWKSET_REFRESH_MIN_INTERVAL
+
+        return expires
